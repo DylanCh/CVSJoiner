cbb2e85916b750653e344b5980f27a890fadabf6
Dylan Chen <hchen004@citymail.cuny.edu>
2016-04-11 21:52:45 -0400

    Added files via upload

---
+++ UnixCVSJoiner.WinForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Com.StellmanGreene.CSVReader;
+
+namespace UnixCSVJoiner.WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CSV Files (*.csv)|*.csv";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                DataTable table = CSVReader.ReadCSVFile(dialog.FileName, true);
+                dataGridView1.DataSource = table;
+            }
+
+        }
+    }
+}
--- UnixCVSJoiner.WinForm/Program.cs
+++ UnixCVSJoiner.WinForm/Program.cs
@@ -1,1 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 
+namespace UnixCSVJoiner.WinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
