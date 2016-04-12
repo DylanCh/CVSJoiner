2ab8e14d70dc933d84517341fc7714699c29a42a
Dylan Chen <hchen004@citymail.cuny.edu>
2016-04-11 21:50:45 -0400

    Added files via upload

---
+++ UnixCVSJoiner.Console/CSVParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UnixCVSJoiner
+{
+    
+    class CSVParser
+    {
+        private List<int> key;
+        private List<string> value1;
+
+        public CSVParser(string path){
+            key = new List<int>();
+            value1 = new List<string>();
+            readCSV(path);
+        }
+
+        private void readCSV(string path)  {
+            StreamReader reader;
+            try
+            {
+                 reader = new StreamReader(File.OpenRead(path));
+                 parseCSV(reader);
+            }
+            catch (FileNotFoundException fNFE) {
+                Console.WriteLine(fNFE.Message);
+            }   
+        }
+
+        private void parseCSV(StreamReader reader){
+            while (!reader.EndOfStream)
+            {
+                try
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    key.Add(int.Parse(values[0]));
+                    value1.Add((values[1].ToString()));
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                }
+            }
+        }
+    }
+}
--- UnixCVSJoiner.Console/Program.cs
+++ UnixCVSJoiner.Console/Program.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
 
+namespace UnixCVSJoiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVParser csvParser1, csvParser2;
+            string path;
+            if (args.Length < 2) // if no path is entered in CLI
+            {
+                Console.WriteLine("Please enter full path of the CSV file");
+                path = Console.ReadLine();
+                csvParser1 = new CSVParser(path);
+                Console.WriteLine("Please enter the CSV file you want to join");
+                path = Console.ReadLine();
+                csvParser2 = new CSVParser(path);
+            }
+
+            else if (args.Length == 2) // if only one file path is entered in CLI
+            {
+                csvParser1 = new CSVParser(args[1]);
+                Console.WriteLine("Please enter the CSV file you want to join");
+                path = Console.ReadLine();
+                csvParser2 = new CSVParser(path);
+            }
+
+            else if (args.Length == 3) {
+                csvParser1 = new CSVParser(args[1]);
+                csvParser2 = new CSVParser(args[2]);
+            }
+        }
+    }
+}
